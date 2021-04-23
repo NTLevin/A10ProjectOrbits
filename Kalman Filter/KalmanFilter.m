aac84bc7d42600dc577593b559165e94a252ae76
AdriCasab <adriano.waitze@gmail.com>
2021-04-23 17:04:50 +0200

    Correction

--- Kalman Filter/KalmanFilter.m
+++ Kalman Filter/KalmanFilter.m
@@ -1,8 +1,8 @@
-dataSet1 = dataone
-dataSet2 = datatwo
-dataSet3 = datathree
+dataSet1 = dataone;
+dataSet2 = datatwo;
+dataSet3 = datathree;
 
-data = table2array(dataone);
+data = table2array(datathree);
 F = table2array(statetransitionmatrix);
 x_0_0 = data(:,1);
 P_0_0 = [100 0 0; 0 100 0; 0 0 100];
@@ -10,11 +10,9 @@ n = width(data);
 Q = [100 0 0;0 100 0;0 0 100];
 R = Q;
 
-
-
 %Prediction Matrix
 x_1_0 = F*x_0_0;
-P_1_0 = F*P_0_0*rot90(F)+Q
+P_1_0 = F*P_0_0*rot90(F)+Q;
 %Kalman Gain
 K_1 = P_1_0*(P_1_0+R)^-1;
 %Update Matrix
@@ -24,16 +22,19 @@ P_1_1 = (eye(3)-K_1)*P_1_0*rot90((eye(3)-K_1))+K_1*R*rot90(K_1);
 x_n_n = x_0_0;
 P_n_n = P_0_0;
 
+datacorrected = [];
+
 for i = 1:n
 %Prediction Matrix
 x_n1_n = F*x_n_n;
+x = x_n_n - x_0_0
 P_n1_n = F*P_n_n*rot90(F)+1;
 %Kalman Gain
 K_n1 = P_n1_n*(P_n1_n+R)^(-1);
 %Update Matrix
 x_n1_n1 = x_n1_n + K_n1*(data(:,(n))-x_n1_n);
 P_n1_n1 = (eye(3)- K_n1)*P_n1_n*rot90(eye(3)-K_n1)+K_n1*R*rot90(K_n1);
-datacorrected = [datacorrected x_n1_n1];
+datacorrected = [datacorrected, x_n1_n1];
 end
 
 timestep = 2*pi*sqrt((6378+500)^3/398600)/3600;
@@ -41,3 +42,12 @@ time = [];
 for j = 1:n
 time = [time timestep*j];
 end
+
+%X1 = datacorrected(1,:);
+%X2 = data(1,:);
+%figure(1)
+%plot(time, X1 ,'red','linewidth',2 )
+%hold all
+%plot(time, X2 ,'green','linewidth',2 )
+%xlabel('Time (sec)');
+%ylabel('X');
