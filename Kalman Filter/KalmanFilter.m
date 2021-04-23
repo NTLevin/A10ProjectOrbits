c6e7db5400cf36f3711af5fbff64a7ae4f641a18
AdriCasab <adriano.waitze@gmail.com>
2021-04-23 16:28:19 +0200

    Loop

--- Kalman Filter/KalmanFilter.m
+++ Kalman Filter/KalmanFilter.m
@@ -23,3 +23,15 @@ P_1_1 = (eye(3)-K_1)*P_1_0*rot90((eye(3)-K_1))+K_1*R*rot90(K_1);
 
 x_n_n = x_0_0;
 P_n_n = P_0_0;
+
+for i = 1:n
+%Prediction Matrix
+x_n1_n = F*x_n_n;
+P_n1_n = F*P_n_n*rot90(F)+1;
+%Kalman Gain
+K_n1 = P_n1_n*(P_n1_n+R)^(-1);
+%Update Matrix
+x_n1_n1 = x_n1_n + K_n1*(data(:,(n))-x_n1_n);
+P_n1_n1 = (eye(3)- K_n1)*P_n1_n*rot90(eye(3)-K_n1)+K_n1*R*rot90(K_n1);
+datacorrected = [datacorrected x_n1_n1];
+end
