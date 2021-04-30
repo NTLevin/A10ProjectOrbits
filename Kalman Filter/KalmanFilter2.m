5768f8a8054e958b4a7be7fd34e33d9010ed88cb
Nathan <N.T.Levin@student.tudelft.nl>
2021-04-30 15:21:04 +0200

    updated plot labels

--- Kalman Filter/KalmanFilter2.m
+++ Kalman Filter/KalmanFilter2.m
@@ -55,84 +55,93 @@ X1 = DATA_one(1,:);
 X2 = dataKalman(1,:);
 X3 = DATA_three(1,:);
 figure(1)
-hold on
-plot(time, X3 ,'red','linewidth',2 )
-plot(time, X1 ,'green','linewidth',2 )
-plot(time, X2 ,'blue','linewidth',2 )
-xlabel('Time (sec)');
-ylabel('X');
-hold off
-
-
-Y1 = DATA_one(2,:);
-Y2 = dataKalman(2,:);
-Y3 = DATA_three(2,:);
-figure(2)
-hold on
-plot(time, Y3 ,'red','linewidth',2 )
-plot(time, Y1 ,'green','linewidth',2 )
-plot(time, Y2 ,'blue','linewidth',2 )
-xlabel('Time (sec)');
-ylabel('Y');
-hold off
-
-
-
-Z1 = DATA_one(3,:);
-Z2 = dataKalman(3,:);
-Z3 = DATA_three(3,:);
-figure(3)
-hold on
-plot(time, Z3 ,'red','linewidth',2 )
-plot(time, Z1 ,'green','linewidth',2 )
-plot(time, Z2 ,'blue','linewidth',2 )
-xlabel('Time (sec)');
-ylabel('Z');
-hold off
-
-
-
-X_error_one = dataKalman(1,:)-DATA_one(1,:);
-X_error_three = dataKalman(1,:)-DATA_three(1,:);
-figure(4)
-hold on
-plot(time, X_error_three ,'red','linewidth',2 )
-plot(time, X_error_one ,'blue','linewidth',2 )
-xlabel('Time (sec)');
-ylabel('Error Kalman wrt. true data');
-hold off
+% hold on
+% plot(time, X3 ,'red','linewidth',2 )
+% plot(time, X1 ,'green','linewidth',2 )
+% plot(time, X2 ,'blue','linewidth',2 )
+% xlabel('Time (sec)');
+% ylabel('X');
+% hold off
+% 
+% 
+% Y1 = DATA_one(2,:);
+% Y2 = dataKalman(2,:);
+% Y3 = DATA_three(2,:);
+% figure(2)
+% hold on
+% plot(time, Y3 ,'red','linewidth',2 )
+% plot(time, Y1 ,'green','linewidth',2 )
+% plot(time, Y2 ,'blue','linewidth',2 )
+% xlabel('Time (sec)');
+% ylabel('Y');
+% hold off
+% 
+% 
+% 
+% Z1 = DATA_one(3,:);
+% Z2 = dataKalman(3,:);
+% Z3 = DATA_three(3,:);
+% figure(3)
+% hold on
+% plot(time, Z3 ,'red','linewidth',2 )
+% plot(time, Z1 ,'green','linewidth',2 )
+% plot(time, Z2 ,'blue','linewidth',2 )
+% xlabel('Time (sec)');
+% ylabel('Z');
+% hold off
+% 
+% 
+% 
+% X_error_one = dataKalman(1,:)-DATA_one(1,:);
+% X_error_three = dataKalman(1,:)-DATA_three(1,:);
+% figure(4)
+% hold on
+% plot(time, X_error_three ,'red','linewidth',2 )
+% plot(time, X_error_one ,'blue','linewidth',2 )
+% xlabel('Time (sec)');
+% ylabel('Error Kalman wrt. true data');
+% hold off
 
 
 
 X1 = dataKalman(1,:);
 X2 = DATA_one(1,:);
+X3 = DATA_three(1,:);
 figure(1)
-plot(time, X1 ,'green','linewidth',2 )
+plot(time, X3 ,'red','linewidth',2 )
 hold all
-plot(time, X2 ,'red','linewidth',2 )
+plot(time, X2 ,'-^y','linewidth',2,'MarkerIndices',1:600:length(X2))
+plot(time, X1 ,'-sb','linewidth',2,'MarkerIndices',1:600:length(X1))
 xlabel('Time (sec)');
 ylabel('J200 X coordinate (km)');
 hold off
-legend({'KF Prediction','True Orbit'},'Location','northwest','Orientation','horizontal')
+legend({'Sensor Data','KF Prediction','True Orbit'},'Location','northwest','Orientation','horizontal')
+title('Orbital Plot of Sensor, True, and Filtered Data of X Axis')
 
 Y1 = dataKalman(2,:);
 Y2 = DATA_one(2,:);
+Y3 = DATA_three(2,:);
 figure(2)
-plot(time, Y1 ,'green','linewidth',2 )
+plot(time, Y3 ,'red','linewidth',2 )
 hold all
-plot(time, Y2 ,'red','linewidth',2 )
+plot(time, Y2 ,'-^y','linewidth',2,'MarkerIndices',1:600:length(Y2))
+plot(time, Y1 ,'-sb','linewidth',2,'MarkerIndices',1:600:length(Y1))
 xlabel('Time (sec)');
 ylabel('J200 Y coordinate (km)');
 hold off
-legend({'KF Prediction','True Orbit'},'Location','northwest','Orientation','horizontal')
+legend({'Sensor Data','KF Prediction','True Orbit'},'Location','northwest','Orientation','horizontal')
+title('Orbital Plot of Sensor, True, and Filtered Data of Y Axis')
 
 Z1 = dataKalman(3,:);
 Z2 = DATA_one(3,:);
+Z3 = DATA_three(3,:);
 figure(3)
-plot(time, Z1 ,'green','linewidth',2)
+plot(time, Z3,'red','linewidth',2);
 hold on
-plot(time, Z2 ,'red','linewidth',2 )
+plot(time, Z2 ,'-^y','linewidth',2,'MarkerIndices',1:600:length(Z2))
+plot(time, Z1 ,'-sb','linewidth',2,'MarkerIndices',1:600:length(Z1))
 xlabel('Time (sec)');
 ylabel('J2000 Z coordinate (km)');
 hold off
-legend({'KF Prediction','True Orbit'},'Location','northwest','Orientation','horizontal')
+legend({'Sensor Data','KF Prediction','True Orbit'},'Location','northwest','Orientation','horizontal')
+title('Orbital Plot of Sensor, True, and Filtered Data of Z Axis')
